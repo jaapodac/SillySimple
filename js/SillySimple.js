1f079f65a969ef467b721475ea57baa1755723d2
J. Apodaca <jaapodac@gmail.com>
2014-02-25 22:10:48 -0700

    Pulled CSS and JS files from AllPurpose project and updated Index file

---
+++ js/SillySimple.js
@@ -0,0 +1,6 @@
+// JavaScript Document
+ 
+    function hello(message){
+        return "hows it going I want to give myself a message which is this: " + message;
+    }
+    document.getElementById("testing").innerHTML = hello("You're silly");
